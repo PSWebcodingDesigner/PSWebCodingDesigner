a112e73038f762c7947ec3691a6e6aaaa27134d0
20JINN <leeyj2346@gmail.com>
2018-07-08 16:32:16 +0900

    javascript 예제 설명 추가, main.js error 주석 처리

--- main.js
+++ main.js
@@ -21,10 +21,10 @@ for (var 순서 = 0; 순서 < 커피종류.length; 순서++){
 //     들어갈요소 = 들어갈요소 + ("<div class='coffee'>" + 커피종류[순서] + "</div>")
 // }
 
-컨테이너.innerHTML = 들어갈요소 + ("커피종류 개수:" + 커피종류.length + "개");
+// 컨테이너.innerHTML = 들어갈요소 + ("커피종류 개수:" + 커피종류.length + "개");
 
 
-=======
+
 console.log("hello world");
 
 var 오오 = ["올","좋당"]
